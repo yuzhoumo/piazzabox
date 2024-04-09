b3c70751fdf979145a266cab35e19bee5d499565
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2024-04-08 18:01:45 -0700

    fix: build configs

--- viewer/tailwind.config.js
+++ viewer/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: [
-    './index.html',
+    './src/**/*.{html,js}',
   ],
+  fontFamily: {
+    sans: ['Graphik', 'sans-serif'],
+    serif: ['Merriweather', 'serif'],
+  },
   theme: {},
   variants: {},
   plugins: [],
